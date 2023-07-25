5c9b008109e6d72c276493aad0c42016fa398f48
Tang20031010 <tangyz666@gmail.com>
2023-07-25 01:35:54 -0700

    index.js file added comment

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+//Summary: Update and get information from the Google Sheet.
+
+//Import modules from 'googleapis' library to access Google Sheets API.
 const { google } = require('googleapis');
 const sheets = google.sheets('v4');
 
-
+//Define the required scope for accessing Google Sheets API.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
+/**
+ * Retrieves an authentication token to access Google Sheets API.
+ * @returns {Promise<String>} - A Promise that resolves to a valid authentication token.
+ */
 async function getAuthToken() {
     const auth = new google.auth.GoogleAuth({
         scopes: SCOPES
@@ -12,7 +19,13 @@ async function getAuthToken() {
     return authToken;
 }
 
-
+/**
+ * Retrieves information about the Google Sheets.
+ * @param {Object} params - An object containing the required parameters.
+ * @param {String} params.spreadsheetId - The ID of the target Google Sheets spreadsheets.
+ * @param {String} params.auth - The authentication token obtained from the 'getAuthToken()'.
+ * @returns {Promise<Object>} - A Promise that resolves to the response containing information about the spreadsheets.
+ */
 async function getSpreadSheet({spreadsheetId, auth}) {
     const res = await sheets.spreadsheets.get({
     spreadsheetId,
@@ -21,16 +34,30 @@ async function getSpreadSheet({spreadsheetId, auth}) {
     return res;
 }
 
-async function getSpreadSheetValues({spreadsheetId, auth, sheetName}) {
+/**
+ * Retrieves values from a column in the Google Sheets spreadsheet.
+ * @param {Object} params - An object containing the required parameters.
+ * @param {String} params.spreadsheetId - The ID of the target Google Sheets spreadsheet.
+ * @param {string} params.auth - The authentication token obtained from `getAuthToken()`.
+ * @param {string} params.ran - The range of the sheet to retrieve values from.
+ * @returns {Promise<Object>} - A Promise that resolves to the response containing the sheet values.
+ */
+async function getSpreadSheetValues({spreadsheetId, auth, ran}) {
     const res = await sheets.spreadsheets.values.get({
     spreadsheetId,
     auth,
-    range: sheetName
+    range: ran
     });
     return res;
 }
 
-
+/**
+ * Updates values in a specific range of a Google Sheets spreadsheet.
+ * @param {string} spreadsheetId - The ID of the target Google Sheets spreadsheet.
+ * @param {string} range - The range of cells to be updated (e.g., 'Sheet1!A1:B2').
+ * @param {Array<Array<any>>} elements - A 2D array containing the values to be updated in the spreadsheet.
+ * @returns {Promise<Object>} - A Promise that resolves to the response containing update information.
+ */
 async function updateSpreadSheetValues(spreadsheetId, range, elements){
 
     const auth = new google.auth.GoogleAuth({
