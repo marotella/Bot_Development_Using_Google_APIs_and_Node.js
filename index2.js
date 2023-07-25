b9bcd7d12700025e98b7a951b4868de69cefd357
Tang20031010 <tangyz666@gmail.com>
2023-07-25 01:41:13 -0700

    index2.js file added comment

--- index2.js
+++ index2.js
@@ -1,13 +1,28 @@
+// Summary: Get information from the Google folder.
+
+//Import modules from 'googleapis' library to access Google Drive API.
 const { google } = require('googleapis');
 
+//Define the required scope for accessing Google Sheets API.
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
 
+/**
+ * Searches for files within a specific folder in Google Drive.
+ * @param {string} folderId - The ID of the target folder in Google Drive.
+ * @returns {Promise<Object>} - A Promise that resolves to the response containing the list of files found in the folder.
+ **/
 async function searchFile(folderId){
+
+    // Create an authentication instance with the specified scopes
     const auth = new google.auth.GoogleAuth({
         scopes: SCOPES
     });
+
+    // Create a Google Drive API instance
     const drives = google.drive({version: 'v3', auth});
+
+     // Perform a files.list request to search for files within the specified folder
     const res = await drives.files.list({
         q: `'${folderId}' in parents`,
         fields: 'nextPageToken, files(id, name, webViewLink)',
